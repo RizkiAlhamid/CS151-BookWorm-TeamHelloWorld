fda32fae09c24a856c3ab60767a1b7c229cbb1fe
amer.amrieh <amerowski@Ammers-MBP.lan>
2023-11-28 21:08:15 -0800

    updated login page and signup page

--- src/Controllers/SignUpPageController.java
+++ src/Controllers/SignUpPageController.java
@@ -13,23 +13,54 @@ public class SignUpPageController {
     // ====== User database here ======
     private UserDAO userDatabase = new UserDAO();
 
+    // Custom Exception Class for Password Validation
+    public class PasswordFormatException extends Exception {
+        public PasswordFormatException(String message) {
+            super(message);
+        }
+    }
+
+    // Method to Validate Password
+    public void validatePassword(String password) throws PasswordFormatException {
+        if (password.length() < 8) {
+            throw new PasswordFormatException("Password must be at least 8 characters long.");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new PasswordFormatException("Password must contain at least one uppercase letter.");
+        }
+        if (!password.matches(".*\\d.*")) {
+            throw new PasswordFormatException("Password must contain at least one number.");
+        }
+        if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
+            throw new PasswordFormatException("Password must contain at least one special character.");
+        }
+    }
+
+
     public SignUpPageController(SignUpPageView view) {
         this.view = view;
         view.setEnterButtonActionListener(new EnterButtonClickListener());
-
     }
 
     private class EnterButtonClickListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String firstName = view.getFirstNameValue();
-            String lastName = view.getLastNameValue();
-            String password = view.getPasswordValue();
-            // ====== Save user data to database here =======
-            User newUser = new User(firstName, lastName, password);
-            JOptionPane.showMessageDialog(view, "Your username is: " + newUser.getUsername());
-            userDatabase.addUser(newUser);
-            view.dispose();
+            try {
+                String firstName = view.getFirstNameValue();
+                String lastName = view.getLastNameValue();
+                String password = view.getPasswordValue();
+
+                // Validate password
+                validatePassword(password);
+
+                // Existing signup logic
+                User newUser = new User(firstName, lastName, password);
+                JOptionPane.showMessageDialog(view, "Your username is: " + newUser.getUsername());
+                userDatabase.addUser(newUser);
+                view.dispose();
+            } catch (PasswordFormatException ex) {
+                JOptionPane.showMessageDialog(view, ex.getMessage());
+            }
         }
     }
 }
